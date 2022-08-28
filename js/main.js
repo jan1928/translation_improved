9a23b6e576b09d14f1f3f284d6d58968ae37c3f6
jan1928 <janmmzus@yahoo.com>
2022-08-28 11:11:12 +0200

    erster commit

--- js/main.js
+++ js/main.js
@@ -7038,9 +7038,13 @@ function translate(sent, value, index) {
     var xhr = new XMLHttpRequest();
     var text = '';
     var back;
-    var API_Key = "AIzaSyDGNl44X0HmIlh0TjzZlXpWX07AnBiJ3hI";
-    //xhr.open("GET", "https://cors-anywhere.herokuapp.com/https://translation.googleapis.com/language/translate/v2?target=en&key=AIzaSyDGNl44X0HmIlh0TjzZlXpWX07AnBiJ3hI&q="+sent, true);
-    xhr.open("GET", "https://translation.googleapis.com/language/translate/v2?target=en&key=AIzaSyDGNl44X0HmIlh0TjzZlXpWX07AnBiJ3hI&q=" + sent, true);
+
+    //Your GOOGLE-TRANSLATE-API-KEY:
+	
+    var API_KEY = "AIzaSyDGNl44X0HmIlh0TjzZlXpWX07AnBiJ3hI";
+
+    //xhr.open("GET", "https://cors-anywhere.herokuapp.com/https://translation.googleapis.com/language/translate/v2?target=en&key="+API_KEY+"&q="+sent, true);
+    xhr.open("GET", "https://translation.googleapis.com/language/translate/v2?target=en&key="+API_KEY+"&q=" + sent, true);
     xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
     xhr.onreadystatechange = function () {
         if (xhr.readyState == 4) {
@@ -7053,7 +7057,7 @@ function translate(sent, value, index) {
     function zudeutsch(text, value, index) {
         var xhr2 = new XMLHttpRequest();
         //xhr2.open("GET", "https://cors-anywhere.herokuapp.com/https://translation.googleapis.com/language/translate/v2?target=de&key=AIzaSyDGNl44X0HmIlh0TjzZlXpWX07AnBiJ3hI&q="+text, true)
-        xhr2.open("GET", "https://translation.googleapis.com/language/translate/v2?target=de&key=AIzaSyDGNl44X0HmIlh0TjzZlXpWX07AnBiJ3hI&q=" + text, true);
+        xhr2.open("GET", "https://translation.googleapis.com/language/translate/v2?target=de&key="+API_KEY+"&q=" + text, true);
         xhr2.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
         xhr2.onreadystatechange = function () {
             if (xhr2.readyState == 4) {
